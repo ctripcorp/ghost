e1d4efe355025ce1691455e012c97de8d341bfa7
phyxdown <wucc@Ctrip.com>
2015-07-21 10:58:52 +0800

    pool interface

---
+++ pool/pool.go
@@ -0,0 +1,20 @@
+package pool
+
+import "net"
+
+//Pool interface describes a connection pool.
+type Pool interface {
+	//Get returns an available(new or reused) connection from the pool.
+	//It blocks when no connection is available.
+	//Closing a connection puts the connection back to the pool.
+	//Duplicated close is endurable as the second close is ignored.
+	Get() (net.Conn, error)
+
+	//Close closes the pool and all its connections.
+	//Call Get() when the pool is closed is counted as an error.
+	Close()
+	
+	//Len returns the current number of connections in the pool,
+	//including those that are in use or free.
+	Len() int
+}
