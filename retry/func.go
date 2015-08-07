323c704d2802362df1bdeb0bc56f594bfec1b32e
phyxdown <1768649634@qq.com>
2015-08-07 15:04:31 +0800

    combine limitation & randomize to Transform func

--- retry/func.go
+++ retry/func.go
@@ -7,11 +7,7 @@ import (
 func Attempt(retries int, firstSleep time.Duration, op Operation) (int, []error) {
 
 	r := &Retry{
-		Randomize:  false,
 		FirstSleep: firstSleep,
-		MinSleep:   0 * time.Second,
-		MaxSleep:   60 * time.Second,
-		Recursion:  Double,
 		Retries:    retries,
 	}
 
--- retry/type.go
+++ retry/type.go
@@ -6,20 +6,19 @@ import (
 
 type Operation func() error
 
+type Transform func(ori time.Duration) (ret time.Duration)
+
 type Recursion func(fir, pre time.Duration) (cur time.Duration)
 
 //a struct to store retry strategy params
 type Retry struct {
-	//switch of randomizing
-	Randomize bool
-
 	//sleep time before first retry
 	FirstSleep time.Duration
-	//range of sleep time
-	MinSleep time.Duration
-	MaxSleep time.Duration
 
-	//a func to compute sleep time
+	//a func to adjust(limit, randomize or tamper) current sleep time
+	Transform Transform
+
+	//a func to compute next sleep time
 	Recursion Recursion
 
 	//max retry time
@@ -38,25 +37,21 @@ func (r Retry) Attempt(op Operation) (retries int, errors []error) {
 	fir := r.FirstSleep
 	sleep := r.FirstSleep
 	for retries < r.Retries {
-		time.Sleep(r.limit(sleep))
+		if r.Transform != nil {
+			time.Sleep(r.Transform(sleep))
+		} else {
+			time.Sleep(sleep)
+		}
 		retries++
 		err = op()
 		if err != nil {
-			sleep = r.Recursion(fir, sleep)
+			if r.Recursion != nil {
+				sleep = r.Recursion(fir, sleep)
+			}
 			errors = append(errors, err)
 		} else {
 			break
 		}
 	}
 	return retries, errors
-}
-
-func (r Retry) limit(d time.Duration) time.Duration {
-	if d > r.MaxSleep {
-		return r.MaxSleep
-	}
-	if d < r.MinSleep {
-		return r.MinSleep
-	}
-	return d
 }
